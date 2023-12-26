a5529c3ff4396be3d18f078a9f69c9217d2ec666
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2023-12-26 08:03:20 +0000

    Add files via upload

---
+++ inputNumberOnlyEntry_tkinter.py
@@ -0,0 +1,14 @@
+from tkinter import *
+from tkinter import ttk
+from tools import *
+
+form = Tk()
+form.geometry("700x500")
+tkcenter(form)
+
+print(inbox("test",True))
+
+fontall(form,"None 20")
+form.mainloop()
+
+ 
